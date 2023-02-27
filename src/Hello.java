3d9120e9f56c4c81fb9be964178504e075d876e4
ANB <testujeanita@gmail.com>
2023-02-27 15:59:59 +0100

    we added A.liena new code block and a boolean and instead of 'isAlien == false' we have '!isAlien'

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,11 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Anita");
-        System.out.print("Hello World");
 
+        boolean isAlien = false;
+        if (!isAlien) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
