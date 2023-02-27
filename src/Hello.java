63f5c4b71055c892505b74c7a6714dd08793442e
ANB <testujeanita@gmail.com>
2023-02-27 21:35:49 +0100

    addind more logical operations

--- src/Hello.java
+++ src/Hello.java
@@ -8,5 +8,15 @@ public class Hello {
             System.out.println("It is not an alien");
             System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore <100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
