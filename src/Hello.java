58a7213f2f6ce4b9a38e837d117ce6904c78c0d8
ANB <testujeanita@gmail.com>
2023-02-27 21:59:10 +0100

    addind OR operator

--- src/Hello.java
+++ src/Hello.java
@@ -14,9 +14,13 @@ public class Hello {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore <100) {
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore <100)) {
             System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <=90)) {
+            System.out.println("Either or both conditions are true");
         }
     }
 }
